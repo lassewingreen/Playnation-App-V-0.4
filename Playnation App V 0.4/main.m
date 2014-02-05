88ce76b0893db87f0819af8508a4d12fea3037c0
Lasse Wingreen <lassewingreen@gmail.com>
2014-02-05 23:37:04 +0100

    Initial Commit

---
+++ Playnation App V 0.4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Playnation App V 0.4
+//
+//  Created by Lasse Wingreen on 05/02/14.
+//  Copyright (c) 2014 Playnation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNAppDelegate class]));
+    }
+}
